82b15f205f0edb2b9ed178a32613bf29ea2090b1
Blaine Rogers <blaine.w.rogers@gmail.com>
2016-10-26 14:36:57 +0100

    Add api/person/ endpoints

--- server.py
+++ server.py
@@ -7,21 +7,53 @@ import uuid
 from flask import Flask, request, abort
 from flask_cors import CORS
 
-CANNED_RESPONSES =\
-        [ ( lambda j: (j['expertise'].lower() == 'artificial intelligence'
-                       and j['role'].lower() == 'supervisor')
-          , [ { 'name': 'Dr. Tim Timson'
-              , 'department': 'Department of Tim Research'
-              , 'email': 'tim@timresearch.ic.ac.uk'
-              , 'info': '/api/person/<key>'
-              } 
-            , { 'name': 'Dr. Timothy Timsworth'
-              , 'department': 'Department of Tim Rights'
-              , 'email': 'tim@timrights.ic.ac.uk'
-              , 'info': '/api/person/<key>'
-              } 
+PEOPLE =\
+        [ { 'name': 'Dr. Tim Timson'
+          , 'department': 'Department of Tim Research'
+          , 'email': 'tim@timresearch.ic.ac.uk'
+          , 'awards': 
+            [ 'Tim Medal 2009'
+            , 'Nobel Prize for Tim Research'
             ]
-          )
+          , 'papers':
+            [ 'https://arXiv.org/abs/1024.01232'
+            , 'https://arXiv.org/abs/1024.01233'
+            , 'https://arXiv.org/abs/1024.01234'
+            , 'https://arXiv.org/abs/1024.01235'
+            , 'https://arXiv.org/abs/1024.01236'
+            , 'https://arXiv.org/abs/1024.01237'
+            , 'https://arXiv.org/abs/1024.01238'
+            , 'https://arXiv.org/abs/1024.01239'
+            , 'https://arXiv.org/abs/1024.01240'
+            ]
+          , 'keywords':
+            { 'learning': 2546
+            , 'machine': 1000
+            , 'ai': 1000
+            , 'Tim': 40
+            }
+          }
+        , { 'name': 'Dr. Timothy Timsworth'
+          , 'department': 'Department of Tim Rights'
+          , 'email': 'tim@timrights.ic.ac.uk'
+          , 'awards': 
+            [ 'Employee of the month June 2011'
+            , "World's best dad"
+            ]
+          , 'papers':
+            [ 'https://arXiv.org/abs/1024.01232'
+            , 'https://arXiv.org/abs/1024.01233'
+            , 'https://arXiv.org/abs/1024.01234'
+            , 'https://arXiv.org/abs/1024.01235'
+            , 'https://arXiv.org/abs/1024.01236'
+            ]
+          , 'keywords':
+            { 'learning': 2546
+            , 'machine': 1000
+            , 'ai': 1000
+            , 'Tim': 40
+            }
+          }
         ]
 
 app = Flask(__name__)
@@ -30,16 +62,33 @@ queries = {}
 
 @app.route('/api/query/submit', methods=['POST'])
 def submit_query():
+    CANNED_RESPONSES =\
+            [ (lambda j: (j['expertise'].lower() == 'artificial intelligence'
+                          and j['role'].lower() == 'supervisor')
+              , [0, 1])
+            , (lambda j: j['expertise'].lower() == 'reinforcement learning'
+              , [1])
+
+            ]
+
     if request.is_json:
         request_json = defaultdict(lambda: '')
         request_json.update(request.get_json())
     else:
         return 'JSON, please.', 415
     query_id = str(uuid.uuid4())
-    for cond, value in CANNED_RESPONSES:
+    for cond, values in CANNED_RESPONSES:
         if cond(request_json):
-            queries[query_id] = value
-    if queries.get(query_id, None) is None:
+            resp = []
+            for i, v in enumerate(values):
+                resp.append({k: PEOPLE[i][k] for k in 
+                             ('name', 'email', 'department')})
+                person = '/api/person/{}/'.format(i)
+                resp[-1]['research_summary'] = person + 'summary'
+                resp[-1]['full_profile'] = person + 'full'
+            queries[query_id] = resp
+            break
+    if query_id not in queries:
         queries[query_id] = []
     response = {
         'success': True,
@@ -51,6 +100,26 @@ def submit_query():
 def get_query(query_id):
     if query_id in queries:
         return json.dumps(queries[query_id])
+    else:
+        abort(404)
+
+@app.route('/api/person/<int:person_id>/summary')
+def person_summary(person_id):
+    if 0 <= person_id < len(PEOPLE):
+        person = PEOPLE[person_id]
+        resp = { 'papers': len(person['papers'])
+               , 'keywords': sorted(list(person['keywords'].keys()))
+               , 'recent_paper': person['papers'][0]
+               , 'full_profile': '/api/person/{}/full'.format(person_id)
+               }
+        return json.dumps(resp)
+    else:
+        abort(404)
+
+@app.route('/api/person/<int:person_id>/full')
+def person_full(person_id):
+    if 0 <= person_id < len(PEOPLE):
+        return json.dumps(PEOPLE[person_id])
     else:
         abort(404)
 
--- server_tests.py
+++ server_tests.py
@@ -10,7 +10,7 @@ class QueryTestCase(unittest.TestCase):
         response = self.app.post(
             '/api/query/submit',
             data=json.dumps(dict(
-                speciality='artificial intelligence',
+                expertise='artificial intelligence',
                 role='supervisor'
             )),
             content_type='application/json'
@@ -24,12 +24,14 @@ class QueryTestCase(unittest.TestCase):
             [ { 'name': 'Dr. Tim Timson'
               , 'department': 'Department of Tim Research'
               , 'email': 'tim@timresearch.ic.ac.uk'
-              , 'info': '/api/person/<key>'
+              , 'research_summary': '/api/person/0/summary'
+              , 'full_profile': '/api/person/0/full'
               } 
             , { 'name': 'Dr. Timothy Timsworth'
               , 'department': 'Department of Tim Rights'
               , 'email': 'tim@timrights.ic.ac.uk'
-              , 'info': '/api/person/<key>'
+              , 'research_summary': '/api/person/1/summary'
+              , 'full_profile': '/api/person/1/full'
               } 
             ]
         )
@@ -46,6 +48,56 @@ class QueryTestCase(unittest.TestCase):
         full_response = self.app.get(data['results'])
         full_data = json.loads(full_response.data.decode('utf-8'))
         self.assertEqual(full_data, [])
+
+class PersonResearchSummaryTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = server.app.test_client()
+
+    def test_good_request(self):
+        response = self.app.get('/api/person/0/summary')
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data,
+            { 'papers': 9
+            , 'keywords': ['Tim', 'ai', 'learning', 'machine']
+            , 'recent_paper': 'https://arXiv.org/abs/1024.01232'
+            , 'full_profile': '/api/person/0/full'
+            }
+        )
+
+class PersonFullTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = server.app.test_client()
+
+    def test_good_request(self):
+        response = self.app.get('/api/person/0/full')
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data,
+            { 'name': 'Dr. Tim Timson'
+            , 'department': 'Department of Tim Research'
+            , 'email': 'tim@timresearch.ic.ac.uk'
+            , 'awards':
+                [ 'Tim Medal 2009'
+                , 'Nobel Prize for Tim Research'
+                ]
+            , 'papers':
+                [ 'https://arXiv.org/abs/1024.01232'
+                , 'https://arXiv.org/abs/1024.01233'
+                , 'https://arXiv.org/abs/1024.01234'
+                , 'https://arXiv.org/abs/1024.01235'
+                , 'https://arXiv.org/abs/1024.01236'
+                , 'https://arXiv.org/abs/1024.01237'
+                , 'https://arXiv.org/abs/1024.01238'
+                , 'https://arXiv.org/abs/1024.01239'
+                , 'https://arXiv.org/abs/1024.01240'
+                ]
+            , 'keywords': 
+                { 'learning': 2546
+                , 'machine': 1000
+                , 'ai': 1000
+                , 'Tim': 40
+                }
+            }
+        )
 
 if __name__ == '__main__':
     unittest.main()
