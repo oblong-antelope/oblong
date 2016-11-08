df5c57255731c580d2774dd54e331bff93604b7e
Mickey Li <Mickeyhli@outlook.com>
2016-11-08 23:24:15 +0000

    More debugging 20

--- extract_expertise.py
+++ extract_expertise.py
@@ -158,11 +158,11 @@ def augment_author(author, words, date):
     (author_profile, status) = dbh.find_profiles({'name':author}) #find profiles
     
     if not status:
-        author_profile = dbh.add_new_profile({'name':author, 'keywords':repr({})}) #if none, insert new
+        author_profile = dbh.add_new_profile({'name':author, 'keywords':repr([])}) #if none, insert new
     
-    print("FINDING PROFILES", author_profile, status)
+    print("FOUND PROFILES", author_profile, status)
 
-    author_words = ast.literal_eval(author_profile['keywords']) #find author's keywords
+    author_words = author_profile['keywords'] #find author's keywords
 
     for word in words:    
         if word not in author_words:
@@ -172,7 +172,7 @@ def augment_author(author, words, date):
             author_words[word] += weighting(word, words, date) #augment old word
 
     x = sorted(author_words.items(), key=lambda t: t[1], reverse=True)   #sorting the words from lowest to highest freq in list
-    author_profile['keywords'] = repr(x) #update the word list in profile (dict converted to string for db)
+    author_profile['keywords'] = x #update the word list in profile (dict converted to string for db)
     print("FINAL AUTHOR_PROFILE", author_profile)
     dbh.update_profile(author_profile['id'], author_profile) #update row in db 
 
