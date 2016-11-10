e22d9a050d693122b5584294d88a73bf2a9f4d1b
Blaine Rogers <blaine.w.rogers@gmail.com>
2016-11-10 10:01:21 +0000

    Added logic to deal with command line arguments for logging.

--- oblong/main.py
+++ oblong/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Handles command line argument parsing and environment variables."""
 import argparse
+import logging
 import os
 
 import database
@@ -15,6 +16,16 @@ parser.add_argument('--host', metavar='IP', default='localhost',
         help='The host to pass to `Flask.run()`')
 parser.add_argument('--port', metavar='PORT', type=int,
         default=HEROKU_PORT, help='The port to pass to `Flask.run()`')
+parser.add_argument('--log-file', metavar='FILE',
+        help='The file to log to.')
+parser.add_argument('--log-level', default='INFO',
+        choices=['critical', 'error', 'warning', 'info', 'debug'],
+        help='The minimum level for displayed log messages.')
 args = parser.parse_args()
+
+kwargs = { 'level': getattr(logging, args.log_level.upper()) }
+if args.log_file:
+    kwargs['filename'] = args.log_file
+logging.basicConfig(**kwargs)
 
 server.app.run(host=args.host, port=args.port)
